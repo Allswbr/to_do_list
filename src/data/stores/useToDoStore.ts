c46d6e1e6cb10ea19b802ab7140d72744fb2dd9b
Oleg Buravtsov <olegburavtsov00@gmail.com>
2022-10-05 14:08:38 +0300

    create store with zustand

--- src/data/stores/useToDoStore.ts
+++ src/data/stores/useToDoStore.ts
@@ -1,5 +1,7 @@
 import create from 'zustand'
 
+import { generateId } from '../helpers'
+
 interface Task {
     id: string;
     title: string;
@@ -12,3 +14,43 @@ interface ToDoStore {
     updateTask: (id: string, title: string) => void;
     removeTask: (id: string) => void;
 }
+
+export const useToDoStore = create<ToDoStore>((set, get) =>({
+        tasks: [
+            {
+                id:'fffffffffff',
+                title: 'Дефолтная таска',
+                createAt: 343243
+            }
+        ],
+        createTask: (title: string) => {
+            const { tasks } = get();
+            const newTask = {
+                id: generateId(),
+                title,
+                createAt: Date.now(),
+            };
+
+            set({
+                tasks: [newTask].concat(tasks),
+            })
+        },
+        updateTask: (id: string, title: string) => {
+            const { tasks }= get();
+            set( {
+                tasks: tasks.map((task) => ({
+                    ...task,
+                    title: task.id === id ? title : task.title,
+                }))
+            })
+        },
+        removeTask: (id: string) => {
+            const { tasks }= get();
+            set( {
+                tasks: tasks.filter((task) => task.id !== id
+                )
+            })
+        },
+    }
+    )
+)
