db215038ebe95d316f9a4bb05bfc80b10dee63ef
Oleg Buravtsov <olegburavtsov00@gmail.com>
2022-10-05 13:22:38 +0300

    added types

---
+++ src/data/stores/useToDoStore.ts
@@ -0,0 +1,14 @@
+import create from 'zustand'
+
+interface Task {
+    id: string;
+    title: string;
+    createAt: number;
+}
+
+interface ToDoStore {
+    tasks: Task[];
+    createTask: (title: string) => void;
+    updateTask: (id: string, title: string) => void;
+    removeTask: (id: string) => void;
+}
