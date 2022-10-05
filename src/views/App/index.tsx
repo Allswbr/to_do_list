6e55e624b5a174dc122fc28dd40b2a169f744d6e
Oleg Buravtsov <olegburavtsov00@gmail.com>
2022-10-05 15:36:20 +0300

    create InputPlus

--- src/views/App/index.tsx
+++ src/views/App/index.tsx
@@ -2,6 +2,9 @@ import React from 'react';
 
 import { useToDoStore } from '../../data/stores/useToDoStore'
 
+import { InputPlus } from '../components/InputPlus'
+
+
 import styles from './index.module.scss'
 
 export const App: React.FC = () => {
@@ -18,14 +21,19 @@ export const App: React.FC = () => {
         ]
     );
 
-    // createTask('newtask');
-
     return (
         <article className={styles.article}>
             <h1 className={styles.articleTitle}>To Do App</h1>
             {/* Секция для добавления */}
             <section className={styles.articleSection}>
-
+                <InputPlus
+                    onAdd = {(title) => {
+                        if (title) {
+                            createTask(title)
+                        }
+                    }
+                    }
+                />
             </section>
             {/* Секция для отображённых списков задач */}
             <section className={styles.articleSection}>
