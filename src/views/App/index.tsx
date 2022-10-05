ef32f1860de17bf8838b6ca80959c5c3f557edd4
Oleg Buravtsov <olegburavtsov00@gmail.com>
2022-10-05 12:43:29 +0300

    added basic layout

--- src/views/App/index.tsx
+++ src/views/App/index.tsx
@@ -1,7 +1,19 @@
 import React from 'react';
 
+import styles from './index.module.scss'
+
 export const App: React.FC = () => {
     return (
-        <article>Name</article>
+        <article className={styles.article}>
+            <h1 className={styles.articleTitle}>To Do App</h1>
+            {/* Секция для добавления */}
+            <section className={styles.articleSection}>
+
+            </section>
+            {/* Секция для отображённых списков задач */}
+            <section className={styles.articleSection}>
+
+            </section>
+        </article>
     );
 }
